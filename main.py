032d35c79e294b66d7d406942e02969ddb4a1a25
pabitra <pabitrapandit2001@gmail.com>
2023-10-31 21:38:29 +0530

    No face Condition changes

--- main.py
+++ main.py
@@ -10,20 +10,18 @@ class Result(BaseModel):
     responseMessage: str = None
     responseCode: int
 
-
-
 @app.post("/images", response_model=Result)
 async def post_images(file1: UploadFile = File(None), file2: UploadFile = File(None)):
     if file1 is None:
-        return Result(match=False, responseCode = status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="File1 parameter Required")
+        return Result(match=False, responseCode=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="File1 parameter Required")
 
     if file2 is None:
-        return Result(match=False, responseCode = status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="File2 parameter Required")
+        return Result(match=False, responseCode=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="File2 parameter Required")
 
     if not file1.filename:
-        return Result(match=False, responseCode = status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="File1 is empty or not Provided.")
+        return Result(match=False, responseCode=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="File1 is empty or not Provided.")
     if not file2.filename:
-        return Result(match=False, responseCode = status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="File2 is empty or not Provided.")
+        return Result(match=False, responseCode=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="File2 is empty or not Provided.")
 
     # Validate file extensions (you can add more allowed extensions if needed)
     allowed_extensions = {"jpg", "jpeg", "png"}
@@ -31,9 +29,9 @@ async def post_images(file1: UploadFile = File(None), file2: UploadFile = File(N
     file2_extension = file2.filename.split(".")[-1].lower()
 
     if file1_extension not in allowed_extensions:
-        return Result(match=False, responseCode = status.HTTP_400_BAD_REQUEST, responseMessage="Invalid File format for file1")
+        return Result(match=False, responseCode=status.HTTP_400_BAD_REQUEST, responseMessage="Invalid File format for file1")
     if file2_extension not in allowed_extensions:
-        return Result(match=False, responseCode = status.HTTP_400_BAD_REQUEST, responseMessage="Invalid File format for file2")
+        return Result(match=False, responseCode=status.HTTP_400_BAD_REQUEST, responseMessage="Invalid File format for file2")
 
     try:
         first_image = face_recognition.load_image_file(file1.file)
@@ -46,12 +44,11 @@ async def post_images(file1: UploadFile = File(None), file2: UploadFile = File(N
         second_face_detected = len(second_face_encodings) > 0
 
         if not first_face_detected and not second_face_detected:
-            return Result(match=False, responseCode = status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="No face detected in both images.")
-        
+            return Result(match=False, responseCode=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="No face detected in both images.")
         elif not first_face_detected:
-            return Result(match=False, responseCode = status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="No face detected in first Image.")
+            return Result(match=False, responseCode=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="No face detected in first Image.")
         elif not second_face_detected:
-            return Result(match=False, responseCode = status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="No face detected in second Image.")
+            return Result(match=False, responseCode=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="No face detected in second Image.")
         else:
             first_face_encoding = first_face_encodings[0]
             second_face_encoding = second_face_encodings[0]
@@ -60,14 +57,13 @@ async def post_images(file1: UploadFile = File(None), file2: UploadFile = File(N
                 [first_face_encoding], second_face_encoding
             )
 
-            return Result(match=result[0], responseMessage="Face match successfully", responseCode = status.HTTP_200_OK)
+            if result[0] == True:
+                return Result(match=True, responseMessage="Face match successfully", responseCode=status.HTTP_200_OK)
+            else:
+                return Result(match=False, responseMessage="No face match found", responseCode=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
 
     except FileNotFoundError:
-            return Result(match=False, responseCode = status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="File not found.")
+        return Result(match=False, responseCode=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, responseMessage="File not found.")
 
     except Exception as e:
-            return Result(match=False, responseCode = status.HTTP_400_BAD_REQUEST, responseMessage=f"Error: {str(e)}")
-    
-
-
-
+        return Result(match=False, responseCode=status.HTTP_400_BAD_REQUEST, responseMessage=f"Error: {str(e)}")
